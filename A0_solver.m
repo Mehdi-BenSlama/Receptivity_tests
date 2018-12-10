8441c4d63bef1176bc1bc0265f0fa44a41564ba1
Mehdi-BenSlama <36711173+Mehdi-BenSlama@users.noreply.github.com>
2018-12-10 19:58:51 +0100

    A0 solver

--- A0_solver.m
+++ A0_solver.m
@@ -1,20 +1,29 @@
-function [FA_0] = A0_solver(A0,X,a,lambda)
+function [FA_0] = A0_solver(A0,X,a)
 
     % Left hand side for A0 equation
     LHS = A0.^2 - X.^2 + 2*a;
     
     % Initialize Right Hand Side (RHS)
     RHS = zeros(size(A0));
+    RHSconstant = gamma(3/4)/sqrt(2)/gamma(5/4);
     
     % Calculate A0 second derivative
-    A0dd = SecondDerivative(A0,X)
+    A0dd = SecondDerivative(A0,X);
     
     % Loop to compute integral
-    for i = 1:length(X)
-        
-
+    for i = 1:length(RHS)-2
+        RHS(i) = sum(...
+            (A0dd(i+1:end-1).*sqrt(X(i+1:end-1)-X(i)) + ...
+            A0dd(i+2:end).*sqrt(X(i+2:end)-X(i)))       ...
+            .*(X(i+2:end)-X(i+1:end-1))/2 ...
+            ) ...
+            + 2*(X(i+1)-X(i))^(1/2)*A0dd(i); % Head term
     end
+    RHS(end-1) = 2*(X(end)-X(end-1))^(1/2)*A0dd(end-1); % Head term
+    RHS(end) = 0;
+    RHS = RHSconstant*RHS;
     
     
+    FA_0 = LHS - RHS;
 end
 
--- Calculate_A0.m
+++ Calculate_A0.m
@@ -2,6 +2,11 @@
 delS = 0.01;
 S = -1+delS:delS:1;
 
+a = 1;
+
 % Real space
 X = tan(pi*S/2)
 
+A0_0 = abs(X)
+
+A0 = fsolve( @(A) A0_solver(A,X,a), 0*A0_0);
